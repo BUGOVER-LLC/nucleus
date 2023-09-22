1bc43c83c35b7881be6b10397db568f7e4aa460c
artak <manukyan.artak.21@gmail.com>
2023-09-23 01:56:40 +0400

    change repository logic

--- src/Abstracts/Models/AuthModel.php
+++ src/Abstracts/Models/AuthModel.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Nucleus\Abstracts\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as LaravelAuthenticatableUser;
-use Nucleus\Traits\FactoryLocatorTrait;
 use Nucleus\Traits\HashedRouteBindingTrait;
 use Nucleus\Traits\HashIdTrait;
 use Nucleus\Traits\HasResourceKeyTrait;
