fade7dff51ef4ed78bd93601333d7b88699177db
artak <manukyan.artak.21@gmail.com>
2023-11-25 15:55:17 +0400

    remove depends criteria interface

--- src/Abstracts/Criterias/Criteria.php
+++ src/Abstracts/Criterias/Criteria.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Nucleus\Abstracts\Criterias;
 
-use Prettus\Repository\Contracts\CriteriaInterface as PrettusCriteria;
-
-abstract class Criteria implements PrettusCriteria
+abstract class Criteria
 {
 
 }
