0d7691a352b84a0521db04212ad1daf79f52dcbf
BUGOVER <manukyan.artak.21@gmail.com>
2025-01-07 12:35:16 +0400

    clean redundant code and minimize

--- src/Abstracts/Value/Value.php
+++ src/Abstracts/Value/Value.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 namespace Nucleus\Abstracts\Value;
 
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Nucleus\Contract\HasResourceKey;
 use Nucleus\Traits\HasResourceKeyTrait;
 
-abstract class Value implements HasResourceKey
+abstract class Value implements HasResourceKey, CastsAttributes
 {
     use HasResourceKeyTrait;
 }
